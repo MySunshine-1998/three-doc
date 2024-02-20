6792ea9cf920ebc92162dd6f7d22ee39165c8c3b
sunyue <849646148@qq.com>
2024-02-20 16:59:28 +0800

    feat: 展示场景和可旋转方块、线条

--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -1,40 +1,49 @@
 <script setup>
-import { ref } from 'vue'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-defineProps({
-  msg: String,
-})
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(0, 0, 10)
+camera.lookAt( 0, 0, 0 );
 
-const count = ref(0)
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.update();
+
+const lineMaterial = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+const points = [];
+points.push( new THREE.Vector3( - 1, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 1, 0 ) );
+points.push( new THREE.Vector3( 1, 0, 0 ) );
+
+const lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
+
+const line = new THREE.Line( lineGeometry, lineMaterial );
+scene.add(line)
+
+function animate() {
+	requestAnimationFrame( animate );
+
+  controls.update();
+	renderer.render( scene, camera );
+}
+animate();
+
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
